6f4eb9932ae71a4222f2012d8fd0d508d06aec92
unknown <haorujiang1997@gmail.com>
2024-01-03 16:31:01 -0800

    add Header, Footer Components

---
+++ Frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import './Footer.css';
+import {FacebookOutlined,TwitterOutlined,YoutubeOutlined } from "@ant-design/icons";
+
+export default function Footer(){
+    return (
+        <div className='footer'>
+            <p style={{color:'white', paddingLeft: '5%'}}>@2022 All Rights Reserved</p>
+            <div className='footer_icons'>
+                <FacebookOutlined style={{color:'white'}}/>
+                <TwitterOutlined style={{color:'white'}}/>
+                <YoutubeOutlined style={{color:'white'}}/>
+            </div>
+            <div className='footer_contacts'>
+                <p style={{color:'white'}}>Contact Us</p>
+                <p style={{color:'white'}}>Privact Policies</p>
+                <p style={{color:'white'}}>Help</p>
+            </div>
+        </div>
+    )
+}
