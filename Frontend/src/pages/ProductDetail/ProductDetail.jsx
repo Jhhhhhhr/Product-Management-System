61a9223eab6f95e0085a0666afb7d5dc6aca2f58
unknown <haorujiang1997@gmail.com>
2024-01-13 01:44:15 -0800

    Update Product Detail Page

--- Frontend/src/pages/ProductDetail/ProductDetail.jsx
+++ Frontend/src/pages/ProductDetail/ProductDetail.jsx
@@ -1,33 +1,59 @@
 import styles from "./ProductDetail.module.css";
-import { Tag, Button } from "antd";
+import { Tag, Button, Space } from "antd";
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { updateCartItem } from "../../features/cart/cartSlice";
 import { fetchOneProductInfo } from "../../services/product";
+import { MinusOutlined, PlusOutlined } from "@ant-design/icons";
 
 export default function ProductDetail() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  
   const { id } = useParams();
   const [detail, setDetail] = useState({});
   const { username, isAdmin, token } = useSelector((state) => state.user.info);
+  const cartItems = useSelector((state) => state.cart.cart.items);
+  const [quantityInCart, setQuantityInCart] = useState();
 
   useEffect(() => {
     fetchOneProductInfo(id)
-    .then((data)=>{
-      setDetail(data);
-    })
-    .catch((error) => console.error("Error fetching data: ", error));
+      .then((data) => {
+        setDetail(data);
+      })
+      .catch((error) => console.error("Error fetching data: ", error));
   }, []);
 
-  const onClickAddCart = () => {
-    dispatch(updateCartItem(token, id, 1));
-  }
+  useEffect(()=>{
+    const cartItem = cartItems.find((item) => item.productID._id === id);
+    const quantity = cartItem ? cartItem.quantity : 0;
+    setQuantityInCart(quantity);
+  },[cartItems]);
 
-  const onClickEditProduct = () =>{
+  const changeQuantityInCart = (type) => async () => {
+    let curQuantity = quantityInCart;
+    if(type==="add"){
+      curQuantity += 1;
+    }
+    else if(type === "sub"){
+      curQuantity -= 1;
+    }
+    else if(type ==="insert"){
+      curQuantity = 1;
+    }
+
+    setQuantityInCart(curQuantity);
+    try {
+      await dispatch(updateCartItem({ token, productID: id, quantity: curQuantity })).unwrap();
+    } catch (e) {
+      alert(e.message);
+    }
+  };
+
+  const onClickEditProduct = () => {
     navigate(`/edit-product/${id}`);
-  }
+  };
 
   return (
     <>
@@ -79,15 +105,36 @@ export default function ProductDetail() {
             </div>
             <p className={styles.pd_desc}>{detail.description}</p>
             <div className={styles.container}>
-              {username && (
-                <Button
-                  type="primary"
-                  style={{ height: "40px", backgroundColor: "#5048E5" }}
-                  onClick={onClickAddCart}
-                >
-                  Add To Cart
-                </Button>
-              )}
+              {username &&
+                (quantityInCart > 0 ? (
+                  <div className={styles.cart_button_container}>
+                    <Button
+                      className={styles.cart_button}
+                      icon={<MinusOutlined />}
+                      onClick={changeQuantityInCart("sub")}
+                    >
+                      {" "}
+                    </Button>
+                    <span className={styles.quantity_in_cart}>
+                      {quantityInCart}
+                    </span>
+                    <Button
+                      className={styles.cart_button}
+                      icon={<PlusOutlined />}
+                      onClick={changeQuantityInCart("add")}
+                    >
+                      {" "}
+                    </Button>
+                  </div>
+                ) : (
+                  <Button
+                    type="primary"
+                    style={{ height: "40px", backgroundColor: "#5048E5" }}
+                    onClick={changeQuantityInCart("insert")}
+                  >
+                    Add To Cart
+                  </Button>
+                ))}
               {isAdmin && (
                 <Button
                   type="primary"
@@ -97,7 +144,6 @@ export default function ProductDetail() {
                     backgroundColor: "#ffffff",
                     border: "1px solid #CCCCCC",
                   }}
-
                   onClick={onClickEditProduct}
                 >
                   Edit
